f5b607911ff22ef7c2ae7c1670f9d7c9da51686a
nicholasc <nicholasc@alleninstitute.org>
2017-11-22 02:58:25 +0000

    updated test

--- tests/test_ephys_pink_noise.py
+++ tests/test_ephys_pink_noise.py
@@ -19,6 +19,8 @@ def decorated_example():
 def test_example(decorated_example):
     """Sample pytest test function with the pytest fixture as an argument.
     """
+    import aibs.ephys_pink_noise
+    from aibs import ephys_pink_noise
 
 
 
